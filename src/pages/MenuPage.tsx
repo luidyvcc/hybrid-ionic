3e3c5bb10222fbacb0d73306131b0303a204f4b2
Your Name <you@example.com>
2020-03-07 16:27:08 -0300

    lesson 3 done

--- src/pages/MenuPage.tsx
+++ src/pages/MenuPage.tsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IonMenu, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonLabel, IonIcon, IonItem, IonAvatar, IonRippleEffect } from '@ionic/react';
 import { person, home } from 'ionicons/icons';
+import { Plugins, CameraPhoto, CameraResultType } from '@capacitor/core'
+
+const { Camera } = Plugins
 
 const MenuPage: React.FC = () => {
+    const [avatar, setAvatar] = useState('')
+
+    const handleChangeAvatar = async () => {
+        const photo: CameraPhoto = await Camera.getPhoto({
+            resultType: CameraResultType.Uri,
+            allowEditing: true
+        })
+        setAvatar(photo.webPath!)
+    }
+
     return (
         <IonMenu type="overlay" contentId="main">
             <IonHeader>
@@ -12,9 +25,12 @@ const MenuPage: React.FC = () => {
             </IonHeader>
             <IonContent>
                 <div style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
-                    <IonAvatar className="ion-activatable"
-                    style={{ overflow: 'hidden', cursor: 'ponter' }}>
-                        <img src="https://pbs.twimg.com/profile_images/3729096119/c0edee92b6a8ea4f37e02b4b0159c6e6_400x400.jpeg"
+                    <IonAvatar
+                        className="ion-activatable"
+                        style={{ overflow: 'hidden', cursor: 'pointer' }}
+                        onClick={handleChangeAvatar}
+                    >
+                        <img src={avatar}
                         alt="User's avatar" />
                         <IonRippleEffect />
                     </IonAvatar>
